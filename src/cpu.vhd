6d9efcbccd308a85c32740eb2165621a48729de6
jacobs <jacobs@mathesio.cz>
2023-10-08 18:07:56 +0200

    FSM

--- src/cpu.vhd
+++ src/cpu.vhd
@@ -55,12 +55,12 @@ architecture behavioral of cpu is
   signal ptr_inc    :   std_logic;
   signal ptr_dec    :   std_logic;
   signal ptr_reset  :   std_logic;
-  signal ptr_addr    :   std_logic_vector(12 downto 0);
+  signal ptr_addr   :   std_logic_vector(12 downto 0);
 
 -- PC REGISTER
   signal pc_inc     :   std_logic;
   signal pc_dec     :   std_logic;
-  signal pc_addr     :   std_logic_vector(12 downto 0);
+  signal pc_addr    :   std_logic_vector(12 downto 0);
 
 -- MX1
   signal MX1_out    :   std_logic_vector(12 downto 0);
@@ -71,11 +71,18 @@ architecture behavioral of cpu is
   signal MX2_in     :   std_logic_vector(7 downto 0);
   signal MX2_inc    :   std_logic;
   signal MX2_dec    :   std_logic;
-  signal MX2_select :   std_logic;
+  signal MX2_select :   std_logic_vector(1 downto 0);
 
 
 -- FSM
-  type fsm_state is (s_start, s_fetch, s_decode, s_ptr_inc, s_ptr_dec, s_val_inc, s_val_dec, s_loop_begin, s_loop_end, s_write, s_read, s_null)
+  type fsm_state is ( s_start, s_fetch, s_decode, -- FSM Operational states
+                      s_ptr_inc, s_ptr_dec,       -- PTR Register states
+                      s_val_inc, s_val_dec,       -- 
+                      s_loop_begin, s_loop_end, 
+                      s_break,
+                      s_write, s_read, 
+                      s_null)
+
   signal pstate : fsm_state :- s_start;
   signal nstate : fsm_state :- s_start;
 
@@ -90,9 +97,9 @@ architecture behavioral of cpu is
             pc_addr <= pc_addr + 1;
           elsif pc_dec = '1' then
             pc_addr <= pc_addr - 1;
-          end if:
-        end if:
-      end process:
+          end if;
+        end if;
+      end process;
 
     -- PTR PROCESS
     ptr: process (CLK, RESET, ptr_inc, ptr_dec, ptr_reset) is
@@ -105,22 +112,119 @@ architecture behavioral of cpu is
           elsif ptr_dec = '1' then
             ptr_addr <= ptr_addr - 1;
           elsif ptr_rst = '1' then
-            ptr_addr
-          end if:
-        end if:
-      end process:
+            ptr_addr <= (others => '0');
+          end if;
+        end if;
+      end process;
 
     -- MX1
-    mx1: process (CLK, MX1_select, ptr_addr, pc_addr) is
+    mx1: process (CLK, RESET, MX1_select, ptr_addr, pc_addr) is
       begin
-        if (MX1_select = '0') then
+        if (RESET = '1') then
+          MX1_out <= '0'; -- CHECK IF VALID
+        elsif (MX1_select = '0') then
           DATA_ADDR <= ptr_addr;
         elsif (MX1_select = '1') then
           DATA_ADDR <= pc_addr;
-        end if:
-      end process:
+        end if;
+      end process;
+
+    -- MX2 
+    mx2: process (CLK, RESET, MX2_select) is
+      begin
+        if (RESET = '1') then
+          MX2_out <= (others => '0');
+        elsif rising_edge(CLK) then
+          case MX2_select is
+            when "00" =>
+              MX2_out <= IN_DATA;
+            when "01" =>
+              MX2_out <= DATA_RDATA - 1;
+            when "10" =>
+              MX2_out <= DATA_RDATA + 1;
+            when others <= (others => '0');
+          end case;
+        end if;
+      end process;
+          
 
 
+    -- FSM
+    state_logic: process (CLK, RESET, EN) is
+      begin
+        if RESET = '1' then
+          pstate <= s_start;
+        elsif rising_edge(CLK) then
+          pstate <= nstate;
+        end if;
+      end process;
+
+    fsm: process (pstate, OUT_BUSY, IN_VLD, DATA_RDATA, CNT)
+      begin
+        cnt_inc    <= '0';
+        cnt_dec    <= '0';
+
+        ptr_inc    <= '0';
+        ptr_dec    <= '0';
+        ptr_reset  <= '0';
+
+        pc_inc     <= '0';
+        pc_dec     <= '0';
+
+        MX1_select <= '0';
+        MX2_select <= "00";
+
+        DATA_RDWR  <= '0';
+        DATA_EN    <= '0';
+        OUT_WE     <= '0';
+        IN_REQ     <= '0';
+
+        case pstate is
+
+          when s_start =>
+            nstate <= s_fetch;
+
+          when s_fetch =>
+            EN <= '1';
+            nstate <= s_decode;
+
+          when s_decode =>
+            case DATA_ADDR is
+              when 0x3E => -- ">"
+                nstate <= s_ptr_inc;
+              when 0x3C => -- "<"
+                nstate <= s_ptr_dec;
+              when 0x2B => -- "+"
+                nstate <= s_val_inc;
+              when 0x2D => -- "-"
+                nstate <= s_val_dec;
+              when 0x5B => -- "["
+                nstate <= s_loop_begin;
+              when 0x5D => -- "]"
+                nstate <= s_loop_end;
+              when 0x7E => -- "~"
+                nstate <= s_break;
+              when 0x2E => -- "."
+                nstate <= s_write;
+              when 0x2C => -- ","
+                nstate <= s_read;
+              when 0x40 => -- "@"
+                nstate <= s_null;
+            end case;
+
+          when s_ptr_inc    =>
+          when s_ptr_dec    =>
+          when s_val_inc    =>
+          when s_val_dec    =>
+          when s_loop_begin =>
+          when s_loop_end   =>
+          when s_break      =>
+          when s_write      =>
+          when s_read       =>
+          when s_null       =>
+        end case;
+      end process;
+          
     --fsm_pstate: process (RESET, CLK)
       --begin
         --if (RESET='1')
